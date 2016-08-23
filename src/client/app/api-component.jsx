685512fe7f6929ff529d41c862bcccb7a1d83ca3
Valay Desai <Valay Desai>
2016-08-23 20:22:20 +0530

    added parameter in api call for unit celsius

--- src/client/app/api-component.jsx
+++ src/client/app/api-component.jsx
@@ -1,6 +1,6 @@
 import 'whatwg-fetch';
 let rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
-let apiUrl = '&appid=398950a8797aa050e609dd1ff1bd0c2f';
+let apiUrl = '&appid=398950a8797aa050e609dd1ff1bd0c2f&units=metric';
 
 export default class Api {
 
